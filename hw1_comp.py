3c99ad393d12984ebeb398c14837a23cf4ae781b
cameroncfranklin <cameroncolbyfranklin@gmail.com>
2022-09-02 15:41:38 -0700

    Computes spearman coefficents for each url match

--- hw1_comp.py
+++ hw1_comp.py
@@ -7,19 +7,48 @@ duckduckgo_json = json.load(duckduckgoResults)
 googleResults = open(
     "/Users/Cameron/Desktop/Homework 2022/CSCI 572/Google_Result4.txt")
 google_json = json.load(googleResults)
-# print(f'DuckDuckGo results: {duckduckgo_json} Google Results: {google_json}')
 
-res = []
-googleRanking = []
-for query in duckduckgo_json:
-    counter = 0
-    length = len(duckduckgo_json[query])
-    for anchor in range(length):
-        if duckduckgo_json[query][anchor] in google_json[query]:
-            counter += 1
-            googleRanking.append(google_json[query].index(
-                duckduckgo_json[query][anchor]))
-    res.append([anchor, counter, counter/10])
 
-print(res)
-print(googleRanking)
+def findURLMatches(json1, json2):
+    matches = []
+    for query in json1:
+        temp = []
+        length = len(json1[query])
+        for url in range(length):
+            if json1[query][url] in json2[query]:           # Found match
+                temp.append([json2[query].index(
+                    json1[query][url]) + 1, url + 1])
+        matches.append(temp)
+    return matches
+
+
+def spearman_cofficient(data):
+    for query in data:
+        n = len(query)
+        for match in query:
+            d = match[0] - match[1]
+            dsquare = d**2
+            if n == 1:
+                if match[0] == match[1]:
+                    coefficient = 1
+                else:
+                    coefficient = 0
+            else:
+                coefficient = 1 - 6*dsquare / (n * (n**2 - 1))
+            match.append(coefficient)
+    print(data)
+
+
+def avg_spearman_coefficent(ranking1, ranking2):
+    d = []
+    n = len(ranking1)
+    for rank in range(len(ranking1)):
+        d.append(ranking2[rank] - ranking1[rank])
+    dsquare = [x**2 for x in d]
+    dsquare_sigma = sum(dsquare)
+    coefficient = 1 - 6*dsquare_sigma / (n * (n**2 - 1))
+    return coefficient
+
+
+results = findURLMatches(duckduckgo_json, google_json)
+spearman_cofficient(results)
